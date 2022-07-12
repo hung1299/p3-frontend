ce8329310a5acd42016e37436e59ba757ae29f72
hung1299 <dotaallstar1999@gmail.com>
2022-07-12 11:19:51 +0700

    minor change

--- src/components/homeComponents/CalltoActionSection.js
+++ src/components/homeComponents/CalltoActionSection.js
@@ -1,17 +1,26 @@
 import React from "react";
 
 const CalltoActionSection = () => {
+  const handleSubmit = e => {
+    e.preventDefault();
+    window.scrollTo({
+      top: 100,
+      left: 0,
+      behavior: "smooth"
+    });
+  }
+
   return (
     <div className="subscribe-section bg-with-black">
       <div className="container">
         <div className="row">
           <div className="col-xs-12">
             <div className="subscribe-head">
-              <h2>DO you need more tips?</h2>
-              <p>Sign up free and get the latest tips.</p>
-              <form className="form-section">
-                <input placeholder="Your Email..." name="email" type="email" />
-                <input value="Yes. I want!" name="subscribe" type="submit" />
+              <h2>Feel-good shopping</h2>
+              <p>Why pay more?</p>
+              <form onSubmit={handleSubmit} className="form-section">
+                {/* <input placeholder="Your Email..." name="email" type="email" /> */}
+                <input style={{ marginLeft: '0px'}} value="Shop now" name="subscribe" type="submit" />
               </form>
             </div>
           </div>
